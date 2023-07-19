897d06afd0b9035ca640910acc7af6c0de434abd
Amelia Davis <ameliaaltman22@gmail.com>
2023-07-19 16:25:44 -0600

    Updateds to navigation including division of code and design projects

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// global Variables 
+const  NavBar = document.querySelector('.uk-navbar-container');
+
+
+
+
+
+
+// Nav Actions and Functions
+
+// function checkSlide(){
+//     NavBar.forEach(NavBar => {
+//     const ChangeSlideAt = (window.scrollY + window.innerHeight)
+//     console.log(ChangeSlideAt)
+//     });
+    
+// }
+
+
+
+
+
+
+// Event Listeners 
+window.addEventListener(scroll, NavSwitch(checkSlide));
